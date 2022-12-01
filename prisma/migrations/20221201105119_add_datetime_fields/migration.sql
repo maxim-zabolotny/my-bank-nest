cac658dd5ee67bda42c410ffd0dba72bf13859fc
Fernando Neto <fernando39357@gmail.com>
2022-12-01 08:10:56 -0300

    feat: add new datetime fields

---
+++ prisma/migrations/20221201105119_add_datetime_fields/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `accounts` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "account_history" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "accounts" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
